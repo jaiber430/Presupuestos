0ee27221bbf996b7bd2f3c75c04a54874fddaca8
Cristian <herneycristian@gmail.com>
2025-09-16 18:14:20 -0500

    registro, alertas y fuentes

--- app/view/dashboard.php
+++ app/view/dashboard.php
@@ -6,6 +6,7 @@
     <title>Menú</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../../public/css/menustyles.css">
