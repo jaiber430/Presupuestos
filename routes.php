e26b7c4201925633e1f873c947cce83fbeff0f3d
FjoseMejia <153628140+FjoseMejia@users.noreply.github.com>
2025-09-17 11:30:18 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -12,9 +12,6 @@ return [
     'login-post' => fn() => (new AuthController())->login($_POST),
     'register-post' => fn() => (new AuthController())->register($_POST),
     'dashboard' => fn()=> (new DashboardController())->index(),
-    // 'aulas'=> fn()=> (new ClassroomController())->index(),
-    
-    // 'create-course-post'=> fn()=> (new CourseController())->createCourse($_POST),
-    // 'create-classroom-post'=> fn()=> (new ClassroomController())->createClassroom($_POST)
+   
 ];
 
