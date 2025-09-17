b3528675db96e1e6ec3044f131955764df36e9c0
Cristian <herneycristian@gmail.com>
2025-09-17 08:37:28 -0500

    404 y fuente

--- app/view/Auth/login.php
+++ app/view/Auth/login.php
@@ -44,11 +44,9 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto+Mono:wght@100..700&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 	<link href="<?= APP_URL ?>css/auth/login.css" rel="stylesheet">
 
@@ -60,9 +58,9 @@
 	</header>
 	
 	<main>
-		<div class="alerts">
-			<div class="container-alert">
-				<p class="alert-text"><?= $message ?? ""?></p>
+		<div class="alerts <?= !empty($message ?? '') ? 'show' : '' ?>">
+			<div class="container-alert <?= !empty($message ?? '') ? 'container-alert-danger' : '' ?>">
+				<p class="alert-text"><?= !empty($message ?? '') ? htmlspecialchars($message) : '' ?></p>
 			</div>
 		</div>
 		<div class="login">			
@@ -131,24 +129,18 @@
 				<div class="row">					
 					<div class="col-md-6">
 						<div class="mb-3">
-							<label for="email-user" class="form-label">Correo Personal</label>
-							<input type="email" class="form-control inputRegister" id="email-user">
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="mb-3">
 							<label for="password-user" class="form-label">Contraseña</label>
 							<input type="password" class="form-control inputRegister" id="password-user">
 						</div>
 					</div>
-				</div>
-				<div class="row">
 					<div class="col-md-6">
 						<div class="mb-3">
 							<label for="password-user" class="form-label">Confirmar Contraseña</label>
 							<input type="password" class="form-control inputRegister" id="password-user-con">
 						</div>
 					</div>
+				</div>
+				<div class="row">
 					<div class="col-md-6">
 						<div class="mb-3">
 							<label for="departmento" class="form-label">Departamento</label>
@@ -157,8 +149,6 @@
 							</select>
 						</div>
 					</div>
-				</div>
-				<div class="row">
 					<div class="col-md-6">
 						<div class="mb-3">
 							<label for="centro" class="form-label">Centro de formación</label>
--- app/view/Auth/send_successful.php
+++ app/view/Auth/send_successful.php
@@ -14,11 +14,10 @@ $email= $_GET["email"];
     <!--Css Bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 	
 	<style>
 		*{
-			font-family: 'Inter', sans-serif
+			font-family: Calibri, 'Segoe UI', Arial, Helvetica, sans-serif;
 		}
 	</style>
 </head>
