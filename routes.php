dccb4ca6286c1aec9e4640f9e7708bca5d931abf
FjoseMejia <153628140+FjoseMejia@users.noreply.github.com>
2025-09-17 11:08:51 -0500

    Listo los dashboard

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 
 use presupuestos\controller\Auth\AuthController;
-// use Klassroom\controller\DashboardController;
+use presupuestos\controller\DashboardController;
 // use Klassroom\controller\CourseController;
 // use Klassroom\controller\ClassroomController;
 // use Klassroom\controller\AssigmentController;
@@ -11,7 +11,7 @@ return [
     'login' => fn() => (new AuthController())->showLogin(),
     'login-post' => fn() => (new AuthController())->login($_POST),
     'register-post' => fn() => (new AuthController())->register($_POST),
-    // 'dashboard' => fn()=> (new DashboardController())->index(),
+    'dashboard' => fn()=> (new DashboardController())->index(),
     // 'aulas'=> fn()=> (new ClassroomController())->index(),
     
     // 'create-course-post'=> fn()=> (new CourseController())->createCourse($_POST),
