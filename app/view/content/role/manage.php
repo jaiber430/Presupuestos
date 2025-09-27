da6ea82d3ce6f8e58e9ea24baeb7f5f5936e409d
FjoseMejia <153628140+FjoseMejia@users.noreply.github.com>
2025-09-27 17:58:04 -0500

    Funcional gestionar usuarios

--- app/view/content/role/manage.php
+++ app/view/content/role/manage.php
@@ -15,23 +15,23 @@
           <td><?= $user['id'] ?></td>
           <td><?= htmlspecialchars($user['email']) ?></td>
           <td>
-            <?php if (!empty($user['es_verificado'])): ?>
-              <span class="badge badge-success">✔ Sí</span>
+            <?php if ($user['es_verificado']== 1): ?>
+              <span class="badge bg-success">✔ Sí</span>
             <?php else: ?>
-              <span class="badge badge-danger">✘ No</span>
+              <span class="badge bg-danger">✘ No</span>
             <?php endif; ?>
           </td>
           <td><?= htmlspecialchars($user['nombre_rol'] ?? '') ?></td>
           <td>
-            <form method="post" action="usuarios/update">
+            <form method="post" action="<?= APP_URL. 'usuarios/update'?>">
               <input type="hidden" name="id" value="<?= $user['id'] ?>">
               <input type="email" name="email" value="<?= htmlspecialchars($user['email']) ?>">
               <label>
-                <input type="checkbox" name="verificado" value="1" <?= !empty($user['es_verificado']) ? 'checked' : '' ?>>
+                <input type="checkbox" name="es_verificado" value="1" <?= !empty($user['es_verificado']) ? 'checked' : '' ?>>
                 Verificado
               </label>
 
-              <select name="role_id" id="role_id" required>
+              <select name="rol_id" id="rol_id" required>
                 <option value="">Seleccione el rol</option>
                 <?php foreach ($roles as $role): ?>
                   <option value="<?= $role['id'] ?>" <?= ($role['id'] == ($user['rol_id'] ?? 0)) ? 'selected' : '' ?>>
